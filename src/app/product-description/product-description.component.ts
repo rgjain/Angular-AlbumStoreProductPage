66c0b189005e439c165177df81888fd4c3b869dd
Rupesh Jain <rjain@empoweredbenefits.com>
2020-06-17 18:30:24 +0530

    Task module 3 commit 1

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { ProductService } from "../product.service";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-product-description',
-  templateUrl: './product-description.component.html',
-  styleUrls: ['./product-description.component.css']
+  selector: "app-product-description",
+  templateUrl: "./product-description.component.html",
+  styleUrls: ["./product-description.component.css"],
 })
 export class ProductDescriptionComponent implements OnInit {
-
-  constructor() { }
+  albumInfo: any;
+  constructor(private _productService: ProductService) {}
 
   ngOnInit() {
+    this._productService
+      .getAlbum(1)
+      .pipe(map((response) => (this.albumInfo = response)))
+      .subscribe();
   }
-
 }
---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+
+import { Http, Response } from "@angular/http";
+import { map } from "rxjs/operators";
+import { Observable } from "rxjs/Observable";
+
+@Injectable()
+export class ProductService {
+  private _albumUrl = "../assets/album.json";
+
+  constructor(private _http: Http) {}
+
+  getAlbum(id: number): Observable<any> {
+    return this._http
+      .get(this._albumUrl)
+      .pipe(map((response) => response.json()));
+  }
+}
